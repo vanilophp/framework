1ecc3dad27b3c214b23717b8289cbe53f3d7228d
Attila Fulop <fulopattila122@gmail.com>
2018-09-22 09:09:53 +0200

    Category module has been added

---
+++ tests/ModulesTest.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Contains the ModulesTest class.
+ *
+ * @copyright   Copyright (c) 2018 Attila Fulop
+ * @author      Attila Fulop
+ * @license     MIT
+ * @since       2018-09-22
+ *
+ */
+
+namespace Vanilo\Framework\Tests;
+
+class ModulesTest extends TestCase
+{
+    /** @test */
+    public function all_vanilo_modules_are_loaded()
+    {
+        $modules = $this->app->concord
+            ->getModules(true)
+            ->keyBy(function ($module) {
+                return $module->getId();
+            });
+
+        $this->assertTrue($modules->has('konekt.app_shell'), 'AppShell module is missing');
+        $this->assertTrue($modules->has('vanilo.cart'), 'Cart module is missing');
+        $this->assertTrue($modules->has('vanilo.category'), 'Category module is missing');
+        $this->assertTrue($modules->has('vanilo.checkout'), 'Checkout module is missing');
+        $this->assertTrue($modules->has('vanilo.order'), 'Order module is missing');
+        $this->assertTrue($modules->has('vanilo.product'), 'Product module is missing');
+        $this->assertTrue($modules->has('vanilo.framework'), 'Framework module is missing');
+    }
+
+}
