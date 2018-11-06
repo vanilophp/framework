f010c27db356f95920f36fdca20c78ac9f422992
Attila Fulop <fulopattila122@gmail.com>
2018-11-06 09:45:34 +0100

    Taxon assignment test has been improved

--- tests/TaxonAssignmentTest.php
+++ tests/TaxonAssignmentTest.php
@@ -19,10 +19,11 @@ use Vanilo\Category\Tests\Dummies\Product;
 class TaxonAssignmentTest extends TestCase
 {
     /** @test */
-    public function taxons_can_be_assigned_to_models()
+    public function a_single_taxon_can_be_assigned_to_models()
     {
         $taxonomy = Taxonomy::create(['name' => 'Category']);
         $taxon    = Taxon::create(['taxonomy_id' => $taxonomy->id, 'name' => 'Jams']);
+
         /** @var Product $product */
         $product = Product::create(['name' => 'Puffin Jam']);
 
@@ -30,6 +31,25 @@ class TaxonAssignmentTest extends TestCase
         $product = $product->fresh();
 
         $this->assertCount(1, $product->taxons);
+        $this->assertEquals('Jams', $product->taxons->first()->name);
+    }
+
+    /** @test */
+    public function multiple_taxons_can_be_assigned_to_models()
+    {
+        $taxonomy = Taxonomy::create(['name' => 'Category']);
+        $jams     = Taxon::create(['taxonomy_id' => $taxonomy->id, 'name' => 'Jams']);
+        $strawberry = Taxon::create(['taxonomy_id' => $taxonomy->id, 'name' => 'Strawberry Products']);
+
+        /** @var Product $product */
+        $product = Product::create(['name' => 'Strawberry Jam']);
+
+        $product->taxons()->saveMany([$jams, $strawberry]);
+        $product = $product->fresh();
+
+        $this->assertCount(2, $product->taxons);
+        $this->assertEquals('Jams', $product->taxons[0]->name);
+        $this->assertEquals('Strawberry Products', $product->taxons[1]->name);
     }
 
     /**
@@ -41,10 +61,11 @@ class TaxonAssignmentTest extends TestCase
     {
         parent::setUpDatabase($app);
 
-        $app['db']->connection()->getSchemaBuilder()->create('products', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->timestamps();
-        });
+        $app['db']->connection()->getSchemaBuilder()->create('products',
+            function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('name');
+                $table->timestamps();
+            });
     }
 }
