fa6a4261c76c4b3046569ce49fa0188e5b776dee
Attila Fulop <fulopattila122@gmail.com>
2018-11-10 23:12:33 +0100

    Added OrderWasCancelled and OrderWasCompleted events

---
+++ src/Events/OrderWasCancelled.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Contains the OrderWasCancelled class.
+ *
+ * @copyright   Copyright (c) 2018 Attila Fulop
+ * @author      Attila Fulop
+ * @license     MIT
+ * @since       2018-11-10
+ *
+ */
+
+namespace Vanilo\Order\Events;
+
+class OrderWasCancelled extends BaseOrderEvent
+{
+}
---
+++ src/Events/OrderWasCompleted.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Contains the OrderWasCompleted class.
+ *
+ * @copyright   Copyright (c) 2018 Attila Fulop
+ * @author      Attila Fulop
+ * @license     MIT
+ * @since       2018-11-10
+ *
+ */
+
+namespace Vanilo\Order\Events;
+
+class OrderWasCompleted extends BaseOrderEvent
+{
+}
