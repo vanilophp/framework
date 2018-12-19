947e61f6ce88328a3947f4acc836fcd2c208cc72
Attila Fulop <fulopattila122@gmail.com>
2018-12-19 16:26:57 +0100

    Added scopes and sluggable behavior to property value model

--- src/Models/PropertyValue.php
+++ src/Models/PropertyValue.php
@@ -11,6 +11,9 @@
 
 namespace Vanilo\Properties\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
+use Cviebrock\EloquentSluggable\SluggableScopeHelpers;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Vanilo\Properties\Contracts\PropertyValue as PropertyValueContract;
@@ -25,6 +28,8 @@ use Vanilo\Properties\Contracts\PropertyValue as PropertyValueContract;
  */
 class PropertyValue extends Model implements PropertyValueContract
 {
+    use Sluggable, SluggableScopeHelpers;
+
     protected $table = 'property_values';
 
     protected $guarded = ['id', 'created_at', 'updated_at'];
@@ -48,11 +53,32 @@ class PropertyValue extends Model implements PropertyValueContract
         return $query->orderBy('priority', 'desc');
     }
 
+    public function scopeByProperty($query, $property)
+    {
+        $id = is_object($property) ? $property->id : $property;
+
+        return $query->where('property_id', $id);
+    }
+
     /**
      * Returns the transformed value according to the underlying type
      */
     public function getValue()
     {
         return $this->property->getType()->transformValue($this->value, $this->settings);
+    }
+
+    public function scopeWithUniqueSlugConstraints(Builder $query, Model $model, $attribute, $config, $slug)
+    {
+        return $query->where('property_id', $model->property->id);
+    }
+
+    public function sluggable(): array
+    {
+        return [
+            'value' => [
+                'source' => 'title'
+            ]
+        ];
     }
 }
