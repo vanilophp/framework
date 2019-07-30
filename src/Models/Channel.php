ffc1f6ae2cc3b3e7f95b68677dd5e235ee1a41bc
Attila Fulop <fulopattila122@gmail.com>
2019-07-30 04:36:45 +0200

    Added base model tests

--- src/Models/Channel.php
+++ src/Models/Channel.php
@@ -16,4 +16,12 @@ use Vanilo\Channel\Contracts\Channel as ChannelContract;
 
 class Channel extends Model implements ChannelContract
 {
+    protected $table = 'channels';
+
+    protected $fillable = ['name', 'slug', 'configuration'];
+
+    protected $casts = [
+        'configuration' => 'array'
+    ];
+
 }
