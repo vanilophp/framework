3f4ab63af6f47af39df2aebb404bcc147a5567a1
Attila Fulop <fulopattila122@gmail.com>
2019-12-21 11:11:58 +0100

    Fixes bug #11 with cart id in session without matching db entry

--- tests/NonexistentCartTest.php
+++ tests/NonexistentCartTest.php
@@ -11,6 +11,8 @@
 
 namespace Vanilo\Cart\Tests;
 
+use Illuminate\Support\Facades\Session;
+use Vanilo\Cart\CartManager;
 use Vanilo\Cart\Facades\Cart;
 
 class NonexistentCartTest extends TestCase
@@ -38,5 +40,19 @@ class NonexistentCartTest extends TestCase
     public function cart_reports_is_empty_for_nonexistent_carts()
     {
         $this->assertTrue(Cart::isEmpty());
+    }
+
+    /** @test */
+    public function cart_id_in_session_without_db_entry_returns_proper_empty_values()
+    {
+        $sessionKey = config(CartManager::CONFIG_SESSION_KEY);
+        session([$sessionKey => 999888777]);
+
+        $this->assertNull(Cart::getUser());
+        $this->assertEquals(0, Cart::itemCount());
+        $this->assertEmpty(Cart::getItems());
+        $this->assertEquals(0, Cart::total());
+        $this->assertFalse(Cart::exists());
+        $this->assertTrue(Cart::doesNotExist());
     }
 }
