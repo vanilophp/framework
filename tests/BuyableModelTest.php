cd24ca733388f8cbe1e013dba047822325693fa1
Attila Fulop <fulopattila122@gmail.com>
2020-05-15 06:05:37 +0200

    Removed remnants of decimals

--- tests/BuyableModelTest.php
+++ tests/BuyableModelTest.php
@@ -13,7 +13,6 @@ namespace Vanilo\Support\Tests;
 
 use PHPUnit\Framework\TestCase;
 use Vanilo\Contracts\Buyable;
-use Vanilo\Contracts\Decimal;
 use Vanilo\Support\Tests\Dummies\HeyBuyMe;
 
 class BuyableModelTest extends TestCase
@@ -26,12 +25,12 @@ class BuyableModelTest extends TestCase
     }
 
     /** @test */
-    public function the_price_method_returns_a_proper_decimal()
+    public function the_price_method_returns_a_float_value()
     {
         $buyable = new HeyBuyMe();
         $buyable->price = 12.44;
 
-        $this->assertIsFloat(Decimal::class, $buyable->getPrice());
+        $this->assertIsFloat($buyable->getPrice());
         $this->assertEquals(12.44, $buyable->getPrice());
     }
 }
