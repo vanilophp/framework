c4e82b8586abf3cb662b3c4ab4ec08b69c26d5a9
Lennart Fischer <github@lambdadigamma.com>
2020-11-01 11:45:45 +0100

    Updated Spatie Media Library to v9 and added update migration

---
+++ src/resources/database/migrations/2020_11_01_010500_upgrade_media_table_to_v9.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpgradeMediaTableToV9 extends Migration
+{
+    public function up()
+    {
+        Schema::table('media', function (Blueprint $table) {
+            $table->json('generated_conversions')->nullable()->after('custom_properties');
+        });
+
+        DB::table('media')->cursor()->each(function ($media) {
+            DB::table('media')->where('id', $media->id)->update([
+                'generated_conversions' => json_encode([])                                                       
+            ]);
+        });
+
+        Schema::table('media', function (Blueprint $table) {
+            $table->json('generated_conversions')->nullable(false)->change();
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('media', function (Blueprint $table) {
+            $table->dropColumn(['generated_conversions']);
+        });
+    }
+}
