48d7d23b8057e546c41f6ff9509efb8725c85d5e
Attila Fulop <fulopattila122@gmail.com>
2020-12-08 15:19:00 +0100

    CS fixes

--- src/resources/database/migrations/2020_12_08_150233_upgrade_media_table_to_v9.php
+++ src/resources/database/migrations/2020_12_08_150233_upgrade_media_table_to_v9.php
@@ -1,10 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Support\Str;
 
 class UpgradeMediaTableToV9 extends Migration
 {
