da53e5e60e634a38c87f3be04d77e5fad353d616
Attila Fulop <1162360+fulopattila122@users.noreply.github.com>
2021-05-14 19:35:02 +0200

    Added @method notations to payment status proxy

--- src/Models/PaymentStatusProxy.php
+++ src/Models/PaymentStatusProxy.php
@@ -16,6 +16,18 @@ namespace Vanilo\Payment\Models;
 
 use Konekt\Concord\Proxies\EnumProxy;
 
+/**
+ * @method static PaymentStatus PENDING()
+ * @method static PaymentStatus AUTHORIZED()
+ * @method static PaymentStatus ON_HOLD()
+ * @method static PaymentStatus PAID()
+ * @method static PaymentStatus PARTIALLY_PAID()
+ * @method static PaymentStatus DECLINED()
+ * @method static PaymentStatus TIMEOUT()
+ * @method static PaymentStatus CANCELLED()
+ * @method static PaymentStatus REFUNDED()
+ * @method static PaymentStatus PARTIALLY_REFUNDED()
+ */
 class PaymentStatusProxy extends EnumProxy
 {
 }
