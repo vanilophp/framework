a602c66ea02eb697d2b6a7e5ea323dd24f27d634
S-DICKSON <52682455+S-DICKSON@users.noreply.github.com>
2021-07-07 09:44:55 +0100

    Update CreatePropertyValue.php
    Removed "property_id" as it is never required as 'hideProperties' === true due to PropertyValueController::create().
    
    Additionally,  PropertyValueController::store() retrieves property_id as route parementer.

--- src/Http/Requests/CreatePropertyValue.php
+++ src/Http/Requests/CreatePropertyValue.php
@@ -20,13 +20,13 @@ class CreatePropertyValue extends FormRequest implements CreatePropertyValueCont
 {
     /**
      * @inheritDoc
+     * @refactored sd@groundwow.com 07-07-2021
      */
     public function rules()
     {
         return [
             'title' => 'required|min:1|max:255',
             'value' => 'nullable|min:1|max:255',
-            'property_id' => 'nullable|exists:properties,id',
             'priority' => 'nullable|integer'
         ];
     }
