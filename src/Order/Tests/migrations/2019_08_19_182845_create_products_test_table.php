8683e47dd2dbd15ac2ceac4dcfae405c4b271aff
Attila Fulop <1162360+fulopattila122@users.noreply.github.com>
2021-09-15 09:49:20 +0200

    Fix class name collision after monorepo

--- src/Order/Tests/migrations/2019_08_19_182845_create_products_test_table.php
+++ src/Order/Tests/migrations/2019_08_19_182845_create_products_test_table.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProductsTable extends Migration
+class CreateProductsTestTable extends Migration
 {
     public function up()
     {
