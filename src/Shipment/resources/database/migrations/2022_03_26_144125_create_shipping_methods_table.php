364141e352278813643eb2925c31e3b4c9bd18c8
Attila Fulop <1162360+fulopattila122@users.noreply.github.com>
2022-05-24 21:52:29 +0200

    Fixed shipping methods migration
    - Thanks @towoju5
    - Fixes vanilophp/shipment#1

--- src/Shipment/resources/database/migrations/2022_03_26_144125_create_shipping_methods_table.php
+++ src/Shipment/resources/database/migrations/2022_03_26_144125_create_shipping_methods_table.php
@@ -11,7 +11,7 @@ return new class () extends Migration {
         Schema::create('shipping_methods', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
-            $table->bigInteger('carrier_id')->nullable();
+            $table->bigInteger('carrier_id')->unsigned()->nullable();
             $table->json('configuration')->nullable();
             $table->timestamps();
 
