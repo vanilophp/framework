767aec0a5777e09dad0062e42d20520b6aeb80f3
Attila Fulop <1162360+fulopattila122@users.noreply.github.com>
2024-04-24 18:18:51 +0300

    Added 4 existing methods to the `ShippingMethod` interface

--- src/Shipment/Contracts/ShippingMethod.php
+++ src/Shipment/Contracts/ShippingMethod.php
@@ -14,6 +14,8 @@ declare(strict_types=1);
 
 namespace Vanilo\Shipment\Contracts;
 
+use Illuminate\Support\Collection;
+use Konekt\Address\Contracts\Zone;
 use Vanilo\Contracts\Configurable;
 use Vanilo\Shipment\Models\ShippingFee;
 
@@ -24,4 +26,12 @@ interface ShippingMethod extends Configurable
     public function getCalculator(): ShippingFeeCalculator;
 
     public function estimate(?object $subject = null): ShippingFee;
+
+    public function isZoneRestricted(): bool;
+
+    public function isNotZoneRestricted(): bool;
+
+    public static function availableOnesForZone(Zone|int $zone): Collection;
+
+    public static function availableOnesForZones(Zone|int ...$zones): Collection;
 }
