55fbf39a852a0785db6c1838a02b0b1c0d3d7efd
Attila Fulop <1162360+fulopattila122@users.noreply.github.com>
2024-07-10 16:33:35 +0300

    Migration fix for SQLite

--- src/Links/resources/database/migrations/2024_05_31_074502_add_root_item_to_link_group_items_table.php
+++ src/Links/resources/database/migrations/2024_05_31_074502_add_root_item_to_link_group_items_table.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class () extends Migration {
@@ -18,9 +19,11 @@ return new class () extends Migration {
 
     public function down(): void
     {
-        Schema::table('link_groups', function (Blueprint $table) {
-            $table->dropForeign('link_groups_root_item_id_foreign');
-        });
+        if ('sqlite' !== DB::connection()->getDriverName()) {
+            Schema::table('link_groups', function (Blueprint $table) {
+                $table->dropForeign('link_groups_root_item_id_foreign');
+            });
+        }
 
         Schema::table('link_groups', function (Blueprint $table) {
             $table->dropColumn('root_item_id');
