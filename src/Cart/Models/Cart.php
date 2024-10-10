c80f1d709d8a8d73fc526ca1c169f07415ac985d
xujiongze <31494901+xujiongze@users.noreply.github.com>
2024-10-10 11:33:49 +0200

    Update Cart.php
    Fix. An error occurred during the second remove. Check if the item still exists before removing

--- src/Cart/Models/Cart.php
+++ src/Cart/Models/Cart.php
@@ -89,8 +89,9 @@ class Cart extends Model implements CartContract
     public function removeProduct(Buyable $product): void
     {
         $item = $this->items()->ofCart($this)->byProduct($product)->first();
-
-        $this->removeItem($item);
+        if($item){
+            $this->removeItem($item);
+        }
     }
 
     /**
