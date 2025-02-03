91f13c9e9fc8c86b03cbca45477f9909dfbf8cf7
Ervin Molnar <58359015+molnarerwin@users.noreply.github.com>
2025-02-03 10:22:51 +0200

    Added GTIN field to products and master_product_variants tables

---
+++ src/Product/resources/database/migrations/2025_02_03_091939_add_gtin_to_products_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('gtin')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('gtin');
+        });
+    }
+};
