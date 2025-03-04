f88d167d723feded3cb283ec4536e38d9b97717a
Attila Fulop <1162360+fulopattila122@users.noreply.github.com>
2025-03-04 12:53:21 +0200

    Standardized the Shipment's TimeUnit enum

--- src/Shipment/Models/TimeUnit.php
+++ src/Shipment/Models/TimeUnit.php
@@ -6,9 +6,31 @@ namespace Vanilo\Shipment\Models;
 
 enum TimeUnit: string
 {
-    case SECONDS = 's';
-    case MINUTES = 'm';
-    case HOURS = 'h';
-    case DAYS = 'd';
-    case WEEKS = 'w';
+    case Seconds = 's';
+    case Minutes = 'm';
+    case Hours = 'h';
+    case Days = 'd';
+    case Weeks = 'w';
+
+    public function label(): string
+    {
+        return match($this)
+        {
+            self::Seconds => __('Seconds'),
+            self::Minutes => __('Minutes'),
+            self::Hours => __('Hours'),
+            self::Days => __('Days'),
+            self::Weeks => __('Weeks'),
+        };
+    }
+
+    public static function choices(): array
+    {
+        $result = [];
+        foreach (self::cases() as $unit) {
+            $result[$unit->value] = $unit->label();
+        }
+
+        return $result;
+    }
 }
