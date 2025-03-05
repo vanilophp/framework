97fe67719f530d56528536229102d4dcf9349c65
Attila Fulop <1162360+fulopattila122@users.noreply.github.com>
2025-03-05 09:12:33 +0200

    Resized the `gtin` field to 14 char length (that's the standard max length)

---
+++ src/Product/resources/database/migrations/2025_03_05_091115_change_product_gtin_field_length.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('gtin', 14)->nullable()->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->string('gtin')->nullable()->change();
+        });
+    }
+};
