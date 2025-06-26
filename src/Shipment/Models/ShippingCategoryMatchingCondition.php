7d2f81127e997b6967c6e9a5ae3a4ca384587e5d
Attila Fulop <1162360+fulopattila122@users.noreply.github.com>
2025-06-26 13:22:24 +0300

    Aligned the test for the new default

--- src/Shipment/Models/ShippingCategoryMatchingCondition.php
+++ src/Shipment/Models/ShippingCategoryMatchingCondition.php
@@ -8,14 +8,17 @@ use Konekt\Enum\Enum;
 use Vanilo\Shipment\Contracts\ShippingCategoryMatchingCondition as ShippingCategoryMatchingConditionContract;
 
 /**
+ * @property-read bool $is_unused
  * @property-read bool $is_none
  * @property-read bool $is_all
  * @property-read bool $is_at_least_one
  *
+ * @method bool isUnused()
  * @method bool isNone()
  * @method bool isAll()
  * @method bool isAtLeastOne()
  *
+ * @method static self UNUSED()
  * @method static self NONE()
  * @method static self ALL()
  * @method static self AT_LEAST_ONE()
--- src/Shipment/Tests/ShippingMethodCategoryTest.php
+++ src/Shipment/Tests/ShippingMethodCategoryTest.php
@@ -2,6 +2,7 @@
 
 declare(strict_types=1);
 
+use PHPUnit\Framework\Attributes\Test;
 use Vanilo\Shipment\Contracts\ShippingCategory as ShippingCategoryContract;
 use Vanilo\Shipment\Contracts\ShippingCategoryMatchingCondition as ShippingCategoryMatchingConditionContract;
 use Vanilo\Shipment\Models\ShippingCategory;
@@ -11,8 +12,7 @@ use Vanilo\Shipment\Tests\TestCase;
 
 class ShippingMethodCategoryTest extends TestCase
 {
-    /** @test */
-    public function a_shipping_method_has_no_shipping_category_by_default()
+    #[Test] public function a_shipping_method_has_no_shipping_category_by_default()
     {
         $method = ShippingMethod::create(['name' => 'Canada Post'])->fresh();
 
@@ -21,8 +21,7 @@ class ShippingMethodCategoryTest extends TestCase
         $this->assertNull($method->getShippingCategory());
     }
 
-    /** @test */
-    public function a_shipping_method_can_have_a_shipping_category()
+    #[Test] public function a_shipping_method_can_have_a_shipping_category()
     {
         $shippingCategory = ShippingCategory::create(['name' => 'Stackable']);
         $method = ShippingMethod::create(['name' => 'Canada Post', 'shipping_category_id' => $shippingCategory->id])->fresh();
@@ -33,19 +32,17 @@ class ShippingMethodCategoryTest extends TestCase
         $this->assertEquals($shippingCategory->id, $method->getShippingCategory()->id);
     }
 
-    /** @test */
-    public function shipping_category_matching_condition_is_none_by_default()
+    #[Test] public function shipping_category_matching_condition_is_none_by_default()
     {
         $method = ShippingMethod::create(['name' => 'Canada Post'])->fresh();
 
         $this->assertInstanceOf(ShippingCategoryMatchingConditionContract::class, $method->shipping_category_matching_condition);
-        $this->assertEquals(ShippingCategoryMatchingCondition::NONE(), $method->shipping_category_matching_condition);
-        $this->assertTrue($method->shipping_category_matching_condition->is_none);
-        $this->assertTrue($method->shipping_category_matching_condition->isNone());
+        $this->assertEquals(ShippingCategoryMatchingCondition::UNUSED(), $method->shipping_category_matching_condition);
+        $this->assertTrue($method->shipping_category_matching_condition->is_unused);
+        $this->assertTrue($method->shipping_category_matching_condition->isUnused());
     }
 
-    /** @test */
-    public function a_shipping_method_can_be_assigned_a_shipping_category_matching_condition()
+    #[Test] public function a_shipping_method_can_be_assigned_a_shipping_category_matching_condition()
     {
         $method = ShippingMethod::create([
             'name' => 'Canada Post',
