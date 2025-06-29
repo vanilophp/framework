1ff7a5828aafc5cb00016e2cba002a0da78f1c79
Attila Fulop <1162360+fulopattila122@users.noreply.github.com>
2025-06-29 19:24:25 +0300

    Allow passing an explicit address to the category checker

--- src/Foundation/Shipping/Method/Eligibility/ShippingZoneChecker.php
+++ src/Foundation/Shipping/Method/Eligibility/ShippingZoneChecker.php
@@ -6,15 +6,21 @@ namespace Vanilo\Foundation\Shipping\Method\Eligibility;
 
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Collection;
+use Konekt\Address\Contracts\Address;
 use Konekt\Address\Models\ZoneScope;
 use Konekt\Address\Query\Zones;
 use Vanilo\Checkout\Contracts\Checkout;
 
 class ShippingZoneChecker implements Checker
 {
+    public function __construct(
+        protected ?Address $address = null
+    ) {
+    }
+
     public function onBuildQuery(Builder $shippingMethodQuery, Checkout $checkout): void
     {
-        $address = $checkout->getShippingAddress();
+        $address = $this->address ?? $checkout->getShippingAddress();
         if (null !== $address?->getCountryCode()) {
             $zones = Zones::withScope(ZoneScope::SHIPPING)->theAddressBelongsTo($address);
             if ($zones->isNotEmpty()) {
