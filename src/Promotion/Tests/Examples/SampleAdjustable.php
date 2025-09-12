cacfa8440b3ff17a15012eb1fd8e0ed3cf59b7cb
Attila Fulop <1162360+fulopattila122@users.noreply.github.com>
2025-09-12 17:31:10 +0300

    Modified the test dummy for cart-based discount compliance

--- src/Promotion/Tests/Examples/SampleAdjustable.php
+++ src/Promotion/Tests/Examples/SampleAdjustable.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace Vanilo\Promotion\Tests\Examples;
 
 use Illuminate\Database\Eloquent\Relations\MorphMany;
+use Illuminate\Support\Collection;
 use Vanilo\Adjustments\Contracts\Adjustable;
 use Vanilo\Adjustments\Contracts\AdjustmentCollection;
 use Vanilo\Adjustments\Support\ArrayAdjustmentCollection;
 use Vanilo\Adjustments\Support\RecalculatesAdjustments;
+use Vanilo\Contracts\CheckoutSubject;
 
-class SampleAdjustable implements Adjustable
+class SampleAdjustable implements Adjustable, CheckoutSubject
 {
     use RecalculatesAdjustments;
 
@@ -40,5 +42,20 @@ class SampleAdjustable implements Adjustable
     public function adjustmentsRelation(): MorphMany
     {
         // TODO: Implement adjustmentsRelation() method.
+    }
+
+    public function getItems(): Collection
+    {
+        return collect();
+    }
+
+    public function itemsTotal(): float
+    {
+        return 0;
+    }
+
+    public function total(): float
+    {
+        return 0;
     }
 }
