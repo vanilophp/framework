49d9d1cd1ddf824d18ec73d5d55b852b6ebfe7c9
Attila Fulop <1162360+fulopattila122@users.noreply.github.com>
2026-05-15 11:04:49 +0300

    Added the update of shipping method's usage count both for line-based and adjustment-based shipping fee modes

--- src/Foundation/Listeners/UpdateSalesFigures.php
+++ src/Foundation/Listeners/UpdateSalesFigures.php
@@ -14,6 +14,7 @@ declare(strict_types=1);
 
 namespace Vanilo\Foundation\Listeners;
 
+use Illuminate\Support\Facades\Log;
 use Vanilo\Contracts\Buyable;
 use Vanilo\Order\Contracts\OrderAwareEvent;
 use Vanilo\Order\Contracts\OrderItem;
@@ -24,15 +25,43 @@ class UpdateSalesFigures
     {
         $order = $event->getOrder();
 
-        foreach ($order->getItems() as $item) {
-            /** @var OrderItem $item */
-            if ($item->product instanceof Buyable) {
-                if ($item->quantity >= 0) {
-                    $item->product->addSale($order->ordered_at ?? $order->created_at, $item->quantity);
-                } else {
-                    $item->product->removeSale(-1 * $item->quantity);
+        $shippingLineDetected = false;
+        $saleDate = $order->ordered_at ?? $order->created_at;
+        try {
+            foreach ($order->getItems() as $item) {
+                /** @var OrderItem $item */
+                if ($item->product instanceof Buyable) {
+                    if ($item->quantity >= 0) {
+                        $item->product->addSale($saleDate, $item->quantity);
+                    } else {
+                        $item->product->removeSale(-1 * $item->quantity);
+                    }
+                    if ($item->product_type === 'shipping_method') {
+                        $shippingLineDetected = true;
+                    }
                 }
             }
+
+            if (!$shippingLineDetected && null !== $order->shipping_method_id && null !== $shippingMethod = $order->shippingMethod) {
+                if ($shippingMethod instanceof Buyable) {
+                    $shippingMethod->addSale($saleDate);
+                } else {
+                    $shippingMethod->update([
+                        'usage_count' => $shippingMethod->usage_count + 1,
+                        'last_usage_at' => $saleDate,
+                    ]);
+                }
+            }
+        } catch (\Throwable $e) {
+            Log::error(
+                sprintf(
+                    'Failed updating the sales figures for order id:%d (%s) in class %s. %s %s',
+                    $order->id,
+                    $order->getNumber(),
+                    $e::class,
+                    $e->getMessage(),
+                )
+            );
         }
     }
 }
